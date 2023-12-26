628df38199fea644af42b942a15f2d3d01cfe780
= <=>
2023-12-26 17:38:15 +0200

    Issue #1: fresh os script

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # AIPyAuto
 An intelligent, automation library for repetitive, time-consuming tasks
+## Linkedin work
+## Facebook birthday
+## My IT - Making fresh OS ready to use in second
+- For ubuntu 
+## A Newsletter 
+- where to go tonight?
+- List to learn
+- **Organized by an LLM
---
+++ src/myIT/run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+apt get update
+apt install git nano vim htop nvtop tmux
+
+# install chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# install vscode
+# wget https://az764295.vo.msecnd.net/stable/7f6ab5485bbc008386c4386d08766667e155244e/code_1.33.1-1554971066_amd64.deb
+# sudo apt install ./<file>.deb
+# extensions to install like: python, runner, copilot, gitlens, docker, ssh, jupyter
+
+# install docker and kubectl
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# install conda
+
+## environment preparation -  if available
+
+# configure battery and power management
